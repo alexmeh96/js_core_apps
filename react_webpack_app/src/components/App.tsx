856ff8eb559951b6793a65e3fcfd0e5cf0371986
Alexandr Meshkov <alex.meh96@mail.ru>
2024-02-06 17:57:41 +0300

    react_webpack_app

---
+++ react_webpack_app/src/components/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import "./App2.scss"
+
+export const App = () => {
+
+    return (
+        <div>
+            <button className="button1">click1</button>
+            <button className="button2">click2</button>
+        </div>
+    );
+};
